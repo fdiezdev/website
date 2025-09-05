76901eaefee4e568cac844766be3c9131e9a8198
Francisco Diez <frandiezdev@gmail.com>
2025-09-05 14:17:12 -0300

    error corrected on header

--- blog/index.php
+++ blog/index.php
@@ -1,12 +1,9 @@
-<?php include("./header.php") ?>
+<?php include("../header.php") ?>
     <div>
         <h3 style="background-color: brown; padding: .5%; color: white;">Recent blog posts</h3>
         <ul>
-            <li><h3>Alzheimers exploratory data analysis</h3><a href="#"><button>Read project</button></a></li>
-            <li><h3>ECG signal digital filter using C++</h3><a href="#"><button disabled>Read project</button></a> &nbsp; under deveplopment!</li>
-            <li><h3>High noise warning device for NICU</h3><a href="#"><button disabled>Read project</button></a> &nbsp; under deveplopment!</li>
-            <li><h3>Shazam clone using Python</h3><a href="#"><button disabled>Read project</button></a> &nbsp; under deveplopment!</li>
+            <li>No posts (yet!)</li>
         </ul>
     </div>
    <hr>
-<?php include("./footer.php") ?>
+<?php include("../footer.php") ?>
