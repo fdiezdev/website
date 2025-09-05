d92302df9c1d01c77e9b847f5c5b6251ec6236bc
Francisco Diez <frandiezdev@gmail.com>
2025-09-05 14:23:19 -0300

    Contact and temp resume pages added

---
+++ contact/index.php
@@ -0,0 +1,11 @@
+<?php include("../header.php") ?>
+    <div>
+        <h3 style="background-color: brown; padding: .5%; color: white; text-align: center">Contact me</h3>
+        <p>
+            <a href="mailto:me@fdiez.com.ar"><button>Email contact</button></a>
+        </p>
+        <p>
+            <a href="https://www.linkedin.com/in/fdiez1/" target="_blank"><button>LinkedIn</button></a>
+        </p>
+    </div>
+<?php include("../footer.php") ?>
---
+++ resume/index.php
@@ -0,0 +1,8 @@
+<?php include("../header.php") ?>
+    <div>
+        <h3 style="background-color: brown; padding: .5%; color: white;">Download my resume</h3>
+        <ul>
+            <li>Working on it</li>
+        </ul>
+    </div>
+<?php include("../footer.php") ?>
